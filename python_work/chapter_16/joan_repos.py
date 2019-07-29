1fe3717b4d4d97cc77a2150f6dbed54c50d65175
Joan Killeen <joan.killeen@bluewin.ch>
2019-07-29 13:57:43 +0200

    Added a function

--- python_work/chapter_16/joan_repos.py
+++ python_work/chapter_16/joan_repos.py
@@ -1,39 +1,21 @@
-import requests
+import github_calls
 
-   # Make an API call and store the response.
+# Find all Joan's repos
 url = 'https://api.github.com/users/joankilleen/repos'
-headers = {'Accept': 'application/vnd.github.v3+json'}
-r = requests.get(url, headers=headers)
+response_dict = github_calls.get_from_gitgub(url=url)
 
-# Store API response in a variable.
-response_dict = r.json()
 
-# Process results.
 repo = response_dict[0]
 for repo in response_dict:
 	repo_name = repo['name']
 	print(f"repo name: {repo_name}")
 for repo in response_dict:
 	repo_name = repo['name']
+
+	# Get the commits for repo "python_examples"
 	if repo_name=='python_examples':
 		commits_url = f"https://api.github.com/repos/joankilleen/{repo_name}/commits"
-		r_commits = requests.get(commits_url, headers=headers)
-		print(f"Commits Status code: {r_commits.status_code}")
-		commits = r_commits.json()
+		commits = github_calls.get_from_gitgub(url=commits_url)
 		for commit in commits:
 			print(f"{commit['sha']}")
-		
-#  	
-# 	
-#           
-#          
-#           print(f"Commits Status code: {r_commits.status_code}")
-#           
-     
-# # repo_name = repo['name']
 
-# # commits_url = f"https://api.github.com/repos/joankilleen/{repo_name}/commits"
-
-# # r_commits = requests.get(commits_url, headers=headers)
-# # print(f"Commits Status code: {r.status_code}")
-# # print(f"{r_commits.json()}")
