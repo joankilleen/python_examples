818d4d4df72480536f9b912b727acf3726963d07
Joan Killeen <joan.killeen@bluewin.ch>
2019-08-01 16:20:14 +0200

    Call to test Webservice

--- python_work/stacktrace_commit_matcher/main.py
+++ python_work/stacktrace_commit_matcher/main.py
@@ -1,5 +1,27 @@
+import requests
 import commits
+
+def execute_test():
+
+	test_url = "http://localhost:8080/async/python"
+	headers = {'Accept': 'application/json'}
+	test_response = requests.get(test_url, headers=headers)
+	if test_response.status_code==200:
+		print("Test succeeded")
+		return
+	print("Test Failed with status: {test_response.status_code}")
+	
+	stacktrace = test_response.json()['stackTrace']
+	for entry in stacktrace:
+		print(entry["className"])
+		print(entry['lineNumber'])
 
 
 repo="AynscWebServiceTester"
 response = commits.get_commits_since_date(repo)
+
+# Call a test rest service which generates a NullPointer
+execute_test()
+
+
+
