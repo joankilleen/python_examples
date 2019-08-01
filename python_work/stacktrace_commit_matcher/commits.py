b08c6179860207df8d2eb93ffa0720bafb862c08
Joan Killeen <joan.killeen@bluewin.ch>
2019-08-01 15:26:21 +0200

    get commits since date

--- python_work/stacktrace_commit_matcher/commits.py
+++ python_work/stacktrace_commit_matcher/commits.py
@@ -2,8 +2,15 @@ from commit import Commit
 from datetime import date
 import github_calls
 
-def get_commits_for_date(repo, commit_date=date.today()):
+def get_commits_since_date(repo, commit_date=date.fromordinal(date.today().toordinal()-1)):
    print(f"{commit_date.strftime('%Y-%m-%d')}")
-   commits_url = f"https://api.github.com/repos/joankilleen/{repo}/commits?commiter-date={commit_date}"
+   commits_url = f"https://api.github.com/repos/joankilleen/{repo}/commits?since={commit_date}"
    print(commits_url)
-   github_calls.get_from_gitgub(url=commits_url)
+   response = github_calls.get_from_gitgub(url=commits_url)
+
+   if response.status_code != 200:
+   	raise Exception(f"Commits could not be read from GitHub {response.status_code}")
+
+   json = response.json()
+   for json_object in json:
+   	print(f"{json_object['sha']}")
--- python_work/stacktrace_commit_matcher/main.py
+++ python_work/stacktrace_commit_matcher/main.py
@@ -1,4 +1,5 @@
 import commits
 
+
 repo="AynscWebServiceTester"
-commits = commits.get_commits_for_date(repo)
+response = commits.get_commits_since_date(repo)
